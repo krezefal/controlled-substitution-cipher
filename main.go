872a427a169b9cfe465f6fb0bbf7333c24ccb99b
Timofey Klimov <klimovtimofeyy@gmail.com>
2021-12-12 19:43:29 +0300

    improve enc/dec algorithm; fix some bugs; make code more clearer

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"math/rand"
+	"time"
 )
 
-func encrypt(rounds int, byteRow []byte, keys [16]uint8, subsTables [32][256]uint8) []byte {
+// 1 subblock = 8 bit; 1 key = 8 bit value; 1 row of substitution table designed for 8 bit value.
+// Amount of keys on 1 round should be = amount of subblocks for better encryption.
+// E.g.: 8 rounds is max available value for 256 capacity of keys and 32 amount of subblocks (256 / 8 = 32)
 
-	var result = make([]byte, len(byteRow))
+const rounds = 8
+const subblocksNum = 32
+const keysCapacity = rounds * subblocksNum
+
+	func encrypt(rounds int, byteRow []byte, keys [keysCapacity]uint8, subsTables [32][256]uint8) []byte {
+
+	result := make([]byte, len(byteRow))
+	copy(result, byteRow)
 
 	for r := 0; r < rounds; r++ {
-		for i := 0; i < len(byteRow); i += 4 {
-			result[i] = byteRow[i] ^ keys[0 + r * 4]
+		for i := 0; i < len(result); i += subblocksNum {
+			result[i] = result[i] ^ keys[r * 32]
 			tableNum := result[i] % 32
-			for j := 1; j < 4; j++ {
-				result[i + j] = byteRow[i + j] ^ keys[j + r * 4]
+			for j := 1; j < subblocksNum; j++ {
+				result[i + j] = result[i + j] ^ keys[j + r * 32]
 				result[i + j] = subsTables[tableNum][result[i + j]]
 				tableNum = (result[i + j] % 32) ^ tableNum
 			}
@@ -24,19 +34,20 @@ func encrypt(rounds int, byteRow []byte, keys [16]uint8, subsTables [32][256]uin
 	return result
 }
 
-func decrypt(rounds int, byteRow []byte, keys [16]uint8, subsTables [32][256]uint8) []byte {
+func decrypt(rounds int, byteRow []byte, keys [keysCapacity]uint8, subsTables [32][256]uint8) []byte {
 
-	var result = make([]byte, len(byteRow))
+	result := make([]byte, len(byteRow))
+	copy(result, byteRow)
 
-	for r := rounds - 1; r >= 0; r++ {
-		for i := 0; i < len(byteRow); i += 4 {
+	for r := rounds - 1; r >= 0; r-- {
+		for i := 0; i < len(result); i += subblocksNum {
 			tableNum := result[i] % 32
-			result[i] = byteRow[i] ^ keys[0 + r * 4]
-			for j := 1; j < 4; j++ {
+			result[i] = result[i] ^ keys[r * 32]
+			for j := 1; j < subblocksNum; j++ {
 				tmp := tableNum
 				tableNum = (result[i + j] % 32) ^ tableNum
-				result[i + j] = subsTables[tmp + 16][result[i + j]]
-				result[i + j] = byteRow[i + j] ^ keys[j + r * 4]
+				result[i + j] = subsTables[(tmp + 16) % 32][result[i + j]]
+				result[i + j] = result[i + j] ^ keys[j + r * 32]
 			}
 		}
 	}
@@ -46,13 +57,17 @@ func decrypt(rounds int, byteRow []byte, keys [16]uint8, subsTables [32][256]uin
 
 func main() {
 
-	rounds := 1
-	var keys [16]uint8 // max 4 rounds with different keys
+	var keys [keysCapacity]uint8
 	var subsTables [32][256]uint8
 
+	seed := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(seed)
+
 	for i := range keys {
-        keys[i] = uint8(rand.Intn(256))
+        keys[i] = uint8(r.Intn(256))
     }
+
+	fmt.Println("ROUND KEYS:", keys)
 
 	for i := 0; i < 16; i++ {
 		availableValues := make([]uint8, 256)
@@ -61,7 +76,7 @@ func main() {
 		}
 		j := 0
 		for len(availableValues) != 0 {
-			randIdx := rand.Intn(len(availableValues))
+			randIdx := r.Intn(len(availableValues))
 			randVal := availableValues[randIdx]
 			availableValues = remove(availableValues, randIdx)
 			subsTables[i][j] = randVal
@@ -70,22 +85,16 @@ func main() {
 	}
 	for i := 16; i < 32; i++ {
 		for j := 1; j < 256; j++ {
-			subsTables[i][subsTables[i][j]] = uint8(j)
+			subsTables[i][subsTables[i - 16][j]] = uint8(j)
 		}
 	}
 
 	img, byteRow := getFile("examples/ford.bmp")
 
 	encRow := encrypt(rounds, byteRow, keys, subsTables)
-	err := saveFile(img, encRow, "examples/enc_res.bmp")
-	if err != nil {
-		log.Println(err.Error())
-	}
+	saveFile(img, encRow, "examples/enc_res.bmp")
 
 	decRow := decrypt(rounds, encRow, keys, subsTables)
-	err = saveFile(img, decRow, "examples/dec_res.bmp")
-	if err != nil {
-		log.Println(err.Error())
-	}
+	saveFile(img, decRow, "examples/dec_res.bmp")
 
 }
